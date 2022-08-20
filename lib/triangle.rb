1bb3d410786ae5ac4bc48298b7dc67bc55fe1187
Brian Wangora <brianwangora@gmail.com>
2022-08-20 11:04:32 +0300

    tests passing

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_reader :num1, :num2, :num3
+
+  def initialize(num1, num2, num3)
+    @num1 = num1
+    @num2 = num2
+    @num3 = num3
+  end
+
+  def kind
+    validate_triangle
+    if num1 == num2 && num2 == num3
+      :equilateral
+    elsif num1 == num2 || num2 == num3 || num1 == num3
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_greater_than_zero?
+    [num1, num2, num3].all?(&:positive?)
+  end
+
+  def valid_triangle_inequality?
+    num1 + num2 > num3 && num1 + num3 > num2 && num2 + num3 > num1
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_greater_than_zero? && valid_triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
+
 end
